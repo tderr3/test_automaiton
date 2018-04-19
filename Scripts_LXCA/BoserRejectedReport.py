35a9f7ef084d374eb92cfc79a8264759c2199dc9
todd derr <tderr@lenovo.com>
2018-04-19 07:04:28 -0400

    Includes logic for searching all bugs, sorting out bad due to rejected, creating report

--- Scripts_LXCA/BoserRejectedReport.py
+++ Scripts_LXCA/BoserRejectedReport.py
@@ -1,4 +1,6 @@
-import requests, warnings
+import requests, warnings, openpyxl
+from openpyxl.styles import Font, Alignment, colors, Border, Side, Style
+
 warnings.filterwarnings('ignore')
 global key, bz
 key='AirXEelVqsNtHp4I3OwavnZpVBcG5OdaKf8zuU0S'
@@ -17,6 +19,7 @@ def checkBugHistory(buglist):
     rejCount = 0
     suspectBugList=[]
     for bug in buglist:
+        print('\n')
         print('******************Checking Bug #'+str(bug)+' now!****************************')
         search=requests.get(bz+'/rest/bug/'+str(bug)+'/history?&api_key='+key,verify=False)
         for change in search.json()['bugs'][0]['history']:
@@ -30,13 +33,49 @@ def checkBugHistory(buglist):
                         rejCount = 0
                         print('        '+str(rejCount)+ ' should be Zero now')
         print('*****Done with Bug# '+str(bug)+' counter is '+str(rejCount)+' Setting counter back to zero now*****')
+        print('\n')
         rejCount = 0
                         
     return suspectBugList
                         
                 
-                
-
+def createReport(badBuglist):
+    print('generating report now')
+    hFont=Font(name='Calibri(body)', size=13, bold=True, color=colors.BLUE)
+    hBorder = Border(bottom=Side(style='thick',color=colors.BLACK))
+    hAlignment = Alignment(horizontal='center')
+    header = Style(font = hFont, border=hBorder, alignment=hAlignment)
+    rowCount = 2
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    sheet['A1'] = 'Bugid'
+    sheet['B1'] = 'Summary'
+    sheet['C1'] = 'Bug Status'
+    sheet['D1'] = 'Product'
+    sheet['E1'] = 'Release'
+    sheet['F1'] = 'Reporter'
+    sheet['G1'] = 'Current Owner'
+    sheet['A1'].style = header
+    sheet['B1'].style = header
+    sheet['C1'].style = header
+    sheet['D1'].style = header
+    sheet['E1'].style = header
+    sheet['F1'].style = header
+    sheet['G1'].style = header
+    for bug in badBuglist:
+        search=requests.get(bz+'/rest/bug/'+str(bug)+'?&api_key='+key,verify=False)
+        sheet['A'+str(rowCount)] = search.json()['bugs'][0]['id']
+        sheet['B'+str(rowCount)].hyperlink = 'https://bz.labs.lenovo.com/show_bug.cgi?id='+str(bug)
+        sheet['B'+str(rowCount)].value = search.json()['bugs'][0]['summary']
+        sheet['B'+str(rowCount)].font= Font(color=colors.BLUE, italic=True)
+        sheet['C'+str(rowCount)] = search.json()['bugs'][0]['status']
+        sheet['D'+str(rowCount)] = search.json()['bugs'][0]['product']
+        sheet['E'+str(rowCount)] = search.json()['bugs'][0]['version']
+        sheet['F'+str(rowCount)] = search.json()['bugs'][0]['creator']
+        sheet['G'+str(rowCount)] = search.json()['bugs'][0]['assigned_to']
+        rowCount = rowCount + 1
+    wb.save('Reject_Report.xlsx')
 list1 = getBugList()
 badBuglist = checkBugHistory(list1)
 print(badBuglist)
+createReport(badBuglist)
